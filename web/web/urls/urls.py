26f93f6cc3d084ff79f6c6ac10c686a65ca4dcc8
gpxlcj <gpxlcj@gmail.com>
2014-10-14 22:42:29 +0800

    add server frame

---
+++ web/web/urls/urls.py
@@ -0,0 +1,18 @@
+from django.conf.urls import patterns, include, url
+
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'web.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+)
+
+urlpatterns = patterns('',
+    url(r'^api/', include('apps.api.url'))
+    url(r'')
+
+)
